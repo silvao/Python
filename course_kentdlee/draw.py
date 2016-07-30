92263360aaba3875201f0cc21091ea44a145696c
silvao <silvao@gmail.com>
2016-07-30 16:15:50 -0400

    My first commit for Python/course_kentdlee

---
+++ course_kentdlee/draw.py
@@ -0,0 +1,6 @@
+from turtle import *
+
+turtle = Turtle()
+screen = turtle.getscreen()
+turtle.forward(200)
+screen.exitonclick()
---
+++ course_kentdlee/square.py
@@ -0,0 +1,7 @@
+def f(x):
+    return x * x
+
+
+x = f(8)
+
+print x
